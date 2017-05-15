b3298f997c107a696a2be7efcd548317d6d4d4b4
Matthew Forshaw <matthewforshaw@gmail.com>
2017-05-15 17:42:28 +0100

    Update run.bat

--- windows/run.bat
+++ windows/run.bat
@@ -1,11 +1,16 @@
+REM Add 7-Zip to the classpath, making the '7z' command available to unzip input data.
 set path=%PATH%;"C:\Program Files\7-Zip"
 
+REM Log the name of the computer running the job.
 echo %computername%
 
+REM Print out the task start time.
 time <nul
 
+REM Extract input data. (Remove this line if this is not applicable for your example.)
 7z x -y Jobs.7z
 
+REM print the directory contents.
 dir
 
 REM Create config file
@@ -16,4 +21,5 @@ REM Running simulation
 
 java -jar cloud.jar config.txt
 
+REM Print out the task completion time.
 time < nul
